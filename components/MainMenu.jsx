154fe3cd0dcf0904e9f985c3483aae9e07173fe2
Robert King <robert.o.king@hotmail.co.uk>
2021-04-07 01:28:34 +0200

    Added: MainMenu component

---
+++ components/MainMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MainMenu = () => {
+  return (
+    <Text>Main Menu!</Text>
+  );
+};
+
+export default MainMenu;
