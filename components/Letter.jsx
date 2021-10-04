fa599cbe3b2f67c13bf73abd8233746887a6c037
Robert King <robert.o.king@hotmail.co.uk>
2021-10-04 23:05:47 +0100

    Added: <Letter> component and wordToLetters() in <Word>

---
+++ components/Letter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Letter = ({letter}) => {
+  return (
+    <Text>{letter}</Text>
+  );
+};
+
+export default Letter;
--- components/Word.jsx
+++ components/Word.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import Letter from './Letter';
+
 const Word = ({word}) => {
+  const wordToLetters = () => {
+    const returnable = [];
+
+    for (let i = 0; i < word.length; i += 1) {
+      returnable.push(<Letter key={i} letter={word[i]} />);
+    }
+
+    return returnable;
+  };
+
   return (
-    <Text>{word}</Text>
+    <View>{wordToLetters()}</View>
   );
 };
 
